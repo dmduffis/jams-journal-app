4ff219df89f61119e9701126651941fffbc2c8d6
Daniel Matthew Duffis <dmduffis@gmail.com>
2024-05-02 12:24:51 -0700

    added series on video page

--- screens/Videos.jsx
+++ screens/Videos.jsx
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import VideoSeriesRow from '../components/VideoSeriesRow'
 
 const Videos = () => {
   return (
-    <View>
-      <Text>Videos</Text>
-    </View>
+    <SafeAreaView>
+      < VideoSeriesRow />
+    </SafeAreaView>
   )
 }
 
