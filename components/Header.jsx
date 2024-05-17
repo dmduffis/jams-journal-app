1105fdaac6765de25818391adc302c105fa053a9
Daniel Matthew Duffis <dmduffis@gmail.com>
2024-05-17 16:38:05 -0400

    header fix

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Ionicons } from '@expo/vector-icons'
 
 const Header = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.jams}>Current Issue</Text>
+      <Text style={styles.jams}>jams</Text>
+      <Ionicons 
+      name = 'search-outline'
+      color= 'white'
+      size = {24}
+      style = {{marginTop: 59}}
+      />
     </View>
   )
 }
@@ -13,17 +20,20 @@ export default Header
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#4a8cb0',
-        height: 360,
-        borderBottomLeftRadius: 25,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        height: 120,
+        paddingLeft: 20,
+        paddingRight: 20,
+        paddingTop: 20,
+        paddingBottom: 0,
+        backgroundColor: '#357db5'
     },
     jams: {
-        display: 'flex',
-        fontFamily: 'sans_regular',
-        fontSize: 24,
+        fontFamily: 'Times',
+        fontSize: 30,
         color: 'white',
-        textAlign: 'left',
-        marginTop: 75,
-        marginLeft: 20,
+        marginTop: 48,
     }
 })
